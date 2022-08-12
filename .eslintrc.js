6a2918d1c899d83bd5df424bb9c0425519ecd293
LisaW <LisaW@ya.ru>
2022-08-12 23:16:45 +0300

    init: skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb', 
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: ['prettier', 'react', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': ['warn', prettierOptions],
+    "object-curly-newline": "warn",
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: { 'prettier/prettier': ['warn', prettierOptions] },
+    },
+  ],
+};
