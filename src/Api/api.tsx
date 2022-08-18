5874e9ceeac1e0c11226897bf7a7c2017d09d203
LisaW <LisaW@ya.ru>
2022-08-18 11:44:02 +0300

    refactor: remove url path from api.tsx to .env

--- src/Api/api.tsx
+++ src/Api/api.tsx
@@ -24,7 +24,7 @@ import {
   Auth,
 } from './api-types';
 
-const PATH_TO_SERVER = 'https://learnwords-rsslang-project.herokuapp.com/';
+const { PATH_TO_SERVER } = process.env;
 
 class Api {
   static token: string;
