0228b652b90a047accbb6361ba89be06d9e92c9d
Sergey Kornienko <madkorney@gmail.com>
2022-08-23 00:49:42 +0300

    feat: implement store state slicers: auth, soundPlay, wordList, gameWordList

---
+++ src/components/store/authSlice.tsx
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from './store';
+import StorageInstance from '../../localStorage';
+
+export type AuthState = {
+  isAuth: boolean;
+  authUserId: string;
+  authUserName: string;
+};
+
+const initialState: AuthState = {
+  isAuth: StorageInstance.token || false,
+  authUserId: StorageInstance.userId || '',
+  authUserName: 'user-name',
+  // TODO get name from API by user_id
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    updateAuth: (state, action: PayloadAction<AuthState>) => {
+      const {
+        payload: { isAuth, authUserId, authUserName },
+      } = action;
+
+      state.isAuth = isAuth;
+      state.authUserId = authUserId;
+      state.authUserName = authUserName;
+    },
+  },
+});
+
+export const { updateAuth } = authSlice.actions;
+
+export const isAuth = (state: RootState) => state.auth.isAuth;
+export const getCurentUserName = (state: RootState) => state.auth.authUserName;
+
+export default authSlice.reducer;
