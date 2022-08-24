5ccba8f1f52565cd1f0b2c59a55384d2248ce05c
Weranika <veronikalopanova@gmail.com>
2022-08-24 19:23:19 +0200

    feat: add footer contetn and styles

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import './footer.scss';
 
 function Footer() {
   return (
     <footer className="footer">
-      <a className="veronika__github-link github-link" href="https://github.com/Weranika" target="_blank" rel="noreferrer">GitHub</a>
-      <a className="veronika__rss-link" href="https://rs.school/js/" target="_blank" rel="noreferrer">RSSchool</a>
+      <div className="footer__rs-logo" />
+      <div className="footer__github-info">
+        <a className="footer__github-link" href="https://github.com/1lisaw" target="_blank" rel="noreferrer">1lisaw</a>
+        <a className="footer__github-link" href="https://github.com/Weranika" target="_blank" rel="noreferrer">Weranika</a>
+        <a className="footer__github-link" href="https://github.com/madkorney" target="_blank" rel="noreferrer">madkorney</a>
+      </div>
       <p className="footer__year">2022</p>
     </footer>
   );
