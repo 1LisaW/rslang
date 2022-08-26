309ba9841cfc333ecb6abb92cf8704cf6a3f6559
LisaW <LisaW@ya.ru>
2022-08-26 13:22:10 +0300

    feat: add dispatch to Tutorial and render cards

--- src/components/Tutorial/tutorial.tsx
+++ src/components/Tutorial/tutorial.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { isAuth, getCurrentUserId } from '../store/authSlice';
+import { AppDispatch } from '../store/store';
+import { fetchWordList } from '../store/wordListFetch';
+import { getWordList } from '../store/wordListSlice';
+import WordCard from '../WordCard/wordCard';
 
 function Tutorial() {
+  const isAuthorized = useSelector(isAuth);
+  const currentUserId = useSelector(getCurrentUserId);
+  const wordList = useSelector(getWordList);
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    dispatch(fetchWordList({ isAuthorized, id: currentUserId }));
+  }, [dispatch, currentUserId]);
   return (
-    <h1>Tutorial</h1>
+    <>
+      <h1>Tutorial</h1>
+      {wordList.wordList.map(item => (
+        <WordCard data={item} />
+      ))}
+    </>
   );
 }
 
