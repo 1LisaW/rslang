ea53b25477f7ed8e02ca669fa07609055c99a96a
LisaW <LisaW@ya.ru>
2022-08-27 14:50:51 +0300

    fix: api for aggregated words

--- src/components/Tutorial/tutorial.tsx
+++ src/components/Tutorial/tutorial.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { isAuth, getCurrentUserId } from '../store/authSlice';
 import { AppDispatch } from '../store/store';
@@ -11,10 +12,28 @@ function Tutorial() {
   const isAuthorized = useSelector(isAuth);
   const currentUserId = useSelector(getCurrentUserId);
   const wordList = useSelector(getWordList);
+
+  const location = useLocation();
+  // console.log(location);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get('page'));
+  const group = Number(searchParams.get('group'));
+  // console.log(page, group, setSearchParams);
+  const paginationHandler = () => {
+    setSearchParams(`page=${2}&group=${2}`);
+  };
+  // const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
-    dispatch(fetchWordList({ isAuthorized, id: currentUserId }));
-  }, [dispatch, currentUserId]);
+    dispatch(
+      fetchWordList({
+        isAuthorized,
+        id: currentUserId,
+        page,
+        group,
+      }),
+    );
+  }, [dispatch, isAuthorized, wordList, location.search]);
   return (
     <>
       <h1>Tutorial</h1>
@@ -23,6 +42,9 @@ function Tutorial() {
           <WordCard data={item} />
         </section>
       ))}
+      <button type="button" onClick={paginationHandler}>
+        Click
+      </button>
     </>
   );
 }
--- src/components/store/wordListFetch.tsx
+++ src/components/store/wordListFetch.tsx
@@ -2,10 +2,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { WordListReducer, WordListState } from './types';
 import Api from '../../Api/api';
-// import { isAuth } from './authSlice';
 
 const emptyWordList: WordListState = { wordList: [] };
-// const isAuthorized = useSelector(isAuth);
 type Params = {
   isAuthorized: boolean;
   id?: string;
@@ -15,24 +13,18 @@ type Params = {
 
 export const fetchWordList = createAsyncThunk('wordList/fetchWordList', async (data: Params) => {
   const { isAuthorized, id, group, page } = data;
-  if (!id) {
-    const response = await Api.getWords({ group, page });
-    return 'status' in response ? {
-      ...emptyWordList,
-    } : {
-      wordList: response,
-    };
-  }
-  if (isAuthorized) {
+  if (isAuthorized && id) {
     const response = await Api.getUserAggregatedWords(id, { group, page });
-    return 'status' in response ? {
+    console.log('getUserAggregatedWords', response);
+
+    return 'error' in response ? {
       ...emptyWordList,
     } : {
-      wordList: response.paginatedResults,
+      wordList: [...response.paginatedResults],
     };
   }
   const response = await Api.getWords({ group, page });
-  return 'status' in response ? {
+  return 'error' in response ? {
     ...emptyWordList,
   } : {
     wordList: response,
