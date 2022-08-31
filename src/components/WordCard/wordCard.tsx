ac9e506dfce660e0b15098825bcaef62b8725a0a
Weranika <veronikalopanova@gmail.com>
2022-08-31 13:37:25 +0200

    refactor: optimazing code in wordCard + add audio file

--- src/components/WordCard/wordCard.tsx
+++ src/components/WordCard/wordCard.tsx
@@ -18,14 +18,17 @@ interface CardInput {
   group:number;
 }
 
-const REACT_APP_PATH_TO_SERVER = 'https://learnwords-rsslang-project.herokuapp.com/';
+const { REACT_APP_PATH_TO_SERVER } = process.env;
 
 function WordCard({ data, isAuth, group }:CardInput) {
   console.log(isAuth, 'isAuth');
 
   const audioButtonHandler = {
     play: true,
-    handler: (play: boolean) => {
+    handler: (play: boolean, file: string) => {
+      console.log(file);
+      const audio = new Audio(file);
+      audio.play();
       console.log(play);
     },
   };
@@ -49,133 +52,72 @@ function WordCard({ data, isAuth, group }:CardInput) {
     value: 25,
   };
 
-  const IMG_PATH = REACT_APP_PATH_TO_SERVER.concat(data.image);
+  const IMG_PATH = REACT_APP_PATH_TO_SERVER?.concat(data.image);
+  const AUDIO_PATH = REACT_APP_PATH_TO_SERVER?.concat(data.audio);
   const groupColorClassName = `group${group}`;
 
   return (
     <StylesProvider injectFirst>
-      {!isAuth ? (
-        <Card
-          className="card-word__container"
-        >
-          <CardMedia
-            className="card-word__img"
-            component="img"
-            image={IMG_PATH}
-            width="50%"
-            title={data.word}
-          />
-          <Box className="card__content">
-            <Box className="card__text">
-              <Box className="card__header">
-                <Box className="card__title">
-                  <Typography gutterBottom variant="h4" component="h2">
-                    {data.word}
-                  </Typography>
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="h2"
-                    className="card__transcription"
-                    color="textSecondary"
-                  >
-                    {data.transcription}
-                  </Typography>
-                </Box>
-                <AudioButton {...audioButtonHandler} />
-              </Box>
-              <Box>
+      <Card
+        className="card-word__container"
+      >
+        <CardMedia
+          className="card-word__img"
+          component="img"
+          image={IMG_PATH}
+          width="50%"
+          title={data.word}
+        />
+        <Box className="card__content">
+          <Box className="card__text">
+            <Box className="card__header">
+              <Box className="card__title">
+                <Typography gutterBottom variant="h4" component="h2">
+                  {data.word}
+                </Typography>
                 <Typography
                   gutterBottom
                   variant="h5"
-                  component="h5"
+                  component="h2"
+                  className="card__transcription"
                   color="textSecondary"
-                  className="card__translate"
                 >
-                  {data.wordTranslate}
+                  {data.transcription}
                 </Typography>
               </Box>
-              <CardContent>
-                <Typography
-                  variant="body1"
-                  dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.textMeaning) }}
-                />
-                <Typography variant="body1" color="textSecondary">
-                  {data.textMeaningTranslate}
-                </Typography>
-              </CardContent>
-              <CardContent>
-                <Typography
-                  variant="body1"
-                  dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.textExample) }}
-                />
-                <Typography variant="body1" color="textSecondary">
-                  {data.textExampleTranslate}
-                </Typography>
-              </CardContent>
+              <AudioButton {...{ ...audioButtonHandler, file: (AUDIO_PATH as string) }} />
             </Box>
+            <Box>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="h5"
+                color="textSecondary"
+                className="card__translate"
+              >
+                {data.wordTranslate}
+              </Typography>
+            </Box>
+            <CardContent>
+              <Typography
+                variant="body1"
+                dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.textMeaning) }}
+              />
+              <Typography variant="body1" color="textSecondary">
+                {data.textMeaningTranslate}
+              </Typography>
+            </CardContent>
+            <CardContent>
+              <Typography
+                variant="body1"
+                dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.textExample) }}
+              />
+              <Typography variant="body1" color="textSecondary">
+                {data.textExampleTranslate}
+              </Typography>
+            </CardContent>
           </Box>
-        </Card>
-      ) : (
-        <Card
-          className="card-word__container"
-        >
-          <CardMedia
-            className="card-word__img"
-            component="img"
-            image={IMG_PATH}
-            width="50%"
-            title={data.word}
-          />
-          <Box className="card__content">
-            <Box className="card__text">
-              <Box className="card__header">
-                <Box className="card__title">
-                  <Typography gutterBottom variant="h4" component="h2">
-                    {data.word}
-                  </Typography>
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="h2"
-                    className="card__transcription"
-                    color="textSecondary"
-                  >
-                    {data.transcription}
-                  </Typography>
-                </Box>
-                <AudioButton {...audioButtonHandler} />
-              </Box>
-              <Box>
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="h5"
-                  color="textSecondary"
-                  className="card__translate"
-                >
-                  {data.wordTranslate}
-                </Typography>
-              </Box>
-              <CardContent>
-                <Typography
-                  variant="body1"
-                  dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.textMeaning) }}
-                />
-                <Typography variant="body1" color="textSecondary">
-                  {data.textMeaningTranslate}
-                </Typography>
-              </CardContent>
-              <CardContent>
-                <Typography
-                  variant="body1"
-                  dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.textExample) }}
-                />
-                <Typography variant="body1" color="textSecondary">
-                  {data.textExampleTranslate}
-                </Typography>
-              </CardContent>
-            </Box>
+          {isAuth ? (
             <Box className={groupColorClassName}>
               <Box className="card__authorized-section">
                 <div className="card__buttons-container">
@@ -185,9 +127,9 @@ function WordCard({ data, isAuth, group }:CardInput) {
                 <CircularProgressWithLabel {...progress} />
               </Box>
             </Box>
-          </Box>
-        </Card>
-      )}
+          ) : ('')}
+        </Box>
+      </Card>
     </StylesProvider>
   );
 }
