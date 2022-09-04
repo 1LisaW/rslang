56936dd97ecb668695c59546e3b04da28b6aec68
Sergey Kornienko <madkorney@gmail.com>
2022-09-04 12:25:48 +0300

    refactor: update simple date getter

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,11 +1,7 @@
-function getSimplifiedDate(date: Date): string {
+export default function getSimplifiedDate(date: Date): string {
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const monthDay = date.getDate().toString().padStart(2, '0');
   const stringifiedDate = `${date.getFullYear()}-${month}-${monthDay}`;
 
   return stringifiedDate;
 }
-
-export default {
-  getSimplifiedDate,
-};
